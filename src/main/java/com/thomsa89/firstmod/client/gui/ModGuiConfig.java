9dd33f75f6b98ec1d5743a446cf3a137b67a4ca7
thomsa89 <thomas.kronpass@gmail.com>
2016-01-21 18:15:39 +0100

    Revert "Added Configuration"
    This reverts commit f7cbff1aa7fa4a29d0e499055f6c393491efe9e1.
    
    # Conflicts:
    #	run/config/splash.properties
    #	run/logs/latest.log
    #	src/main/java/com/thomsa89/firstmod/FirstMod.java
    #	src/main/java/com/thomsa89/firstmod/handler/ConfigurationHandler.java

--- src/main/java/com/thomsa89/firstmod/client/gui/ModGuiConfig.java
+++ src/main/java/com/thomsa89/firstmod/client/gui/ModGuiConfig.java
@@ -6,9 +6,6 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfig;
-import net.minecraftforge.fml.client.config.IConfigElement;
-
-import java.util.List;
 
 public class ModGuiConfig extends GuiConfig{
     public ModGuiConfig(GuiScreen guiScreen) {
